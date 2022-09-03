52149ebd4d1a78e51f38b5b245ee04347a7548e0
kebin20 <ktanzyl@hotmail.com>
2022-09-03 17:17:55 +0900

    Added function to be able to draw in different colours, as well as a reset function. Not sure on why the hold mouse click to draw function does not work

--- app.js
+++ app.js
@@ -1,54 +1,58 @@
-let color = 'black';
+let color = "black";
+let click = true;
 
 function populateBoard(size) {
-let grid = document.querySelector('.grid-container');
-let newSquares = grid.querySelectorAll('div');
+  let grid = document.querySelector(".grid-container");
+  let newSquares = grid.querySelectorAll("div");
+  //To change the size of the board
+  newSquares.forEach((div) => div.remove());
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//To change the size of the board
-// newSquares.forEach((div) => div.remove());(NOT sure about this?? )
-grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-let amount = size*size;
-for (let i = 0; i < amount; i++) {
-    let newSquares = document.createElement('div');
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let newSquares = document.createElement("div");
     // grid.appendChild(newSquares); Why is this not needed to work??
-    newSquares.classList.add('addColor');
+    newSquares.classList.add("addColor");
 
-newSquares.addEventListener('mouseover', colorSquare);
+    newSquares.addEventListener("mouseover", colorSquare);
 
-    newSquares.style.backgroundColor = 'white';
-    grid.insertAdjacentElement("beforeend", newSquares)
-  };
+    newSquares.style.backgroundColor = "white";
+    grid.insertAdjacentElement("beforeend", newSquares);
+  }
 }
 
 populateBoard(16);
 
-
 function changeSize(input) {
-    if(input >= 2 && input <= 100 ) {
-        populateBoard(input);
-    } else {
-        alert("Please enter valid size");
-    }
- 
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    alert("Please enter valid size");
+  }
 }
 
 function colorSquare() {
-        this.style.backgroundColor = color;
+  if (color === "random") {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return (this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`);
+  } else {
+    this.style.backgroundColor = color;
+  }
 }
 
-
-
-function changeColor() {
-
+function changeColor(choice) {
+  color = choice;
 }
 
-// grid.addEventListener('mouseover', makeRandColor);
-// function makeRandColor(e) {
-//   let newColor = e.target;
-//   const r = Math.floor(Math.random()*255);
-//   const g = Math.floor(Math.random()*255);
-//   const b = Math.floor(Math.random()*255);
-//   return newColor.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-// }
+function resetBoard() {
+  let grid = document.querySelector(".grid-container");
+  let newSquares = grid.querySelectorAll("div");
+  newSquares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+document.querySelector("body").addEventListener("click", () => {
+  click = !click;
+});
